fbab058761c638b7170242c94e4799b97db8331d
Aditya Penmesta <Aditya@macbook-pro.lan>
2020-06-07 11:32:02 -0400

    Added comments, fixed counter issue

--- poker/poker/models.py
+++ poker/poker/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Table Model
 class Tables(models.Model):
-    code = models.CharField(max_length=16)
-    players = models.ManyToManyField(User)
+    code = models.CharField(max_length=16) #Room Code can be maximum 16 characters long
+    players = models.ManyToManyField(User) #The players for each table, many to many field allows multiple users to be on one table
 
     def __str__(self):
-        return "Table Code: {0}, Players: {1}".format(self.code,self.players.all())
-    
+        return "Table Code: {0}, Players: {1}".format(self.code,self.players.all()) #toStr method 
--- poker/poker/views.py
+++ poker/poker/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from poker.models import Tables
 # Create your views here.
+counter = 0
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "startbootstrap-landing-page-gh-pages/index.html", {"message": "Please Log in"})
@@ -15,36 +16,37 @@ def index(request):
 
 
 def loginScreen(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
+    username = request.POST["username"] #get the username from the HTML page
+    password = request.POST["password"] #get the password from the HTML page
+    user = authenticate(request, username=username, password=password) #check if the user is in the Users database
+    if user is not None: #if this user is authenticated, log them in
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("index")) #go back to main page
     else:
-        return render(request, "startbootstrap-landing-page-gh-pageslogin/index.html", {"message": "Please Login Again"})
+        return render(request, "startbootstrap-landing-page-gh-pageslogin/index.html", {"message": "Please Login Again"}) #if not authenticated, do it again
 
 def logoutScreen(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 def load(request):
-    return render(request, "register.html")
+    return render(request, "register.html") #purpose of this function is to load the register page
 
 def registerScreen(request):
-    first_name = request.POST["first_name"]
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = User.objects.create_user(first_name=first_name, username=username, password=password)
+    first_name = request.POST["first_name"] #get the first name from the HTML page
+    username = request.POST["username"] #get the username from the HTML page
+    password = request.POST["password"] #get the password from the HTML page
+    user = User.objects.create_user(first_name=first_name, username=username, password=password) #create a new user from the information given
     user.save()
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("index")) #go back to the main page to log in again
 
 def createRoom(request):
-    user = request.user
-    counter = 3
-    tables = Tables(code="{0}".format(counter))
+    user = request.user  #get the current user logged in
+    global counter
+    counter = counter + 1 #increment the counter variable every time a room is created
+    tables = Tables(code="{0}".format(counter)) #Create new table, with counter being the new room code
     tables.save()
-    tables.players.add(user)
+    tables.players.add(user) #add the user who created the table to the table
     tables.save()
     context = {
         "tables": tables,
@@ -53,12 +55,12 @@ def createRoom(request):
     return render(request, "createRoom.html", context)
 
 def joinRoom(request):
-    roomCode = request.POST["roomCode"]
-    tables = Tables.objects.all()
+    roomCode = request.POST["roomCode"] #get the room code
+    tables = Tables.objects.all() #get all the tables
     for table in tables:
-        if table.code is roomCode:
-            user = request.user
-            table.players.add(user)
+        if table.code is roomCode:  #if room code is one of the created table's room code
+            user = request.user #get the current user logged in
+            table.players.add(user) #add the user to the table
             table.save()
             context = {
                 "tables": table,
