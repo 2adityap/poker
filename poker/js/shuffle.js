14251392983f08b3d9a92a2a5ac71639b5535b18
Vraj Desai <vraj.desai429@gmail.com>
2020-07-21 18:36:30 -0400

    changes made to table UI and poker algo

--- poker/js/shuffle.js
+++ poker/js/shuffle.js
@@ -1,3 +1,9 @@
+const cards = ["1 Spades", "2 spades", "3 spades", "4 spades", "5 spades", "6 spades","7 spades", "8 spades","9 spades","10 spades",
+"Jack Spades", "Queen Spades", "Kings Spades", "1 Clubs", "2 Clubs", "3 Clubs", "4 Clubs", "5 Clubs", "6 Clubs","7 Clubs", "8 Clubs","9 Clubs","10 Clubs",
+"Jack Clubs", "Queen Clubs", "Kings Clubs", "1 Diamonds", "2 Diamonds", "3 Diamonds", "4 Diamonds", "5 Diamonds", "6 Diamonds","7 Diamonds", "8 Diamonds","9 Diamonds","10 Diamonds",
+"Jack Diamonds", "Queen Diamonds", "Kings Diamonds", "1 Heart", "2 Heart", "3 Heart", "4 Heart", "5 Heart", "6 Heart","7 Heart", "8 Heart","9 Heart","10 Heart",
+"Jack Heart", "Queen Heart", "Kings Heart"];
+
 //Random shuffle method, just takes 2 random numbers and swaps them, works pretty well
 function shuffle1(cards) {
     var i;
@@ -16,7 +22,12 @@ function shuffle2(cards) {
       swap(cards,a,i)
     }
 }
-
+nextCard = () => {
+  var position = 0; 
+  var card = cards[position];
+  position++;
+  return card;
+};
 //swap helper function
 function swap(cards, a, b) {
   let temp = cards[a]
@@ -31,11 +42,6 @@ function shuffle3(cards) {
 
 
 
-const cards = ["1 Spades", "2 spades", "3 spades", "4 spades", "5 spades", "6 spades","7 spades", "8 spades","9 spades","10 spades",
-"Jack Spades", "Queen Spades", "Kings Spades", "1 Clubs", "2 Clubs", "3 Clubs", "4 Clubs", "5 Clubs", "6 Clubs","7 Clubs", "8 Clubs","9 Clubs","10 Clubs",
-"Jack Clubs", "Queen Clubs", "Kings Clubs", "1 Diamonds", "2 Diamonds", "3 Diamonds", "4 Diamonds", "5 Diamonds", "6 Diamonds","7 Diamonds", "8 Diamonds","9 Diamonds","10 Diamonds",
-"Jack Diamonds", "Queen Diamonds", "Kings Diamonds", "1 Heart", "2 Heart", "3 Heart", "4 Heart", "5 Heart", "6 Heart","7 Heart", "8 Heart","9 Heart","10 Heart",
-"Jack Heart", "Queen Heart", "Kings Heart"];
 //console.log(cards.length)
 //callShuffle1(cards)
 //console.log(cards)
